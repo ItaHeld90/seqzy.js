4bcfa09256c6133a77fa8b8b99ca5c059af61fa1
Itamar Held <ita@gmail.com>
2018-06-09 14:33:25 +0300

    move directories

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const wrapIterable = require('./src/wrappers/iterable');
-const wrapMap = require('./src/wrappers/map');
-const wrapSet = require('./src/wrappers/set');
-const wrapString = require('./src/wrappers/string');
-const wrapList = require('./src/wrappers/list');
-const { isIterable } = require('./src/helper-utils');
+const wrapIterable = require('./wrappers/iterable');
+const wrapMap = require('./wrappers/map');
+const wrapSet = require('./wrappers/set');
+const wrapString = require('./wrappers/string');
+const wrapList = require('./wrappers/list');
+const { isIterable } = require('./helper-utils');
 
 const wrap = (iterableObj, construct) => {
     if (!isIterable(iterableObj)) {
