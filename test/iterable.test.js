142cacee37c07c9aa7b6b7ea3f3a50f59530b3a3
Itamar Held <ita@gmail.com>
2018-06-10 01:40:27 +0300

    add mocha

---
+++ test/iterable.test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const seq = require('../src/app');
+
+describe('iterable', () => {
+    it('should work', () => {
+        expect(true).to.be.true;
+    })
+});
